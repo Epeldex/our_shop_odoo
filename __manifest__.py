d8891335f1cec5f9b553e8bd5ab2be6a6448bbe0
2dam <2dam@TartangaLH.eus>
2024-01-11 08:47:55 +0100

    supplier view for odoo

--- __manifest__.py
+++ __manifest__.py
@@ -9,6 +9,10 @@ This module allows to manage onboardings and their progress
     """,
     'depends': ['base'],
     'installable': True,
-    'data': [],
+    'data': ['security/our_shop_security.xml',
+             'security/ir.model.access.csv',
+             'views/views.xml',
+             'views/supplier.xml',
+             'views/supplier_view.xml'],
     'license': 'LGPL-3',
 }
